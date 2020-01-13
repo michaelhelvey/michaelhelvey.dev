b6a0228ba31eb3bff47695041b05961b55a3982c
Michael Helvey <michael.helvey1@gmail.com>
2020-01-12 21:39:41 -0600

    fix global css hopefully

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        ignore: ['blog-post.css', 'react-toggle/style.css'], // Ignore files/folders
+        ignore: ['blog-post.css', 'react-toggle/style.css', 'global.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
