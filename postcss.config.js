08e1a3debdab750ee7c38ac82ba1712893aaf511
Michael Helvey <michael.helvey1@gmail.com>
2020-05-10 08:02:24 -0500

    fix postcss build script

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,17 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.{ts,tsx}"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
-
 module.exports = {
   plugins: [
     "tailwindcss",
     "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            content: ["./src/**/*.{ts,tsx}"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
     "postcss-preset-env",
     "postcss-nesting",
   ],
